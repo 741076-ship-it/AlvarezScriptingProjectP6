186d4b28610580d076e9a11c84ddfdc202a6b721
741076-ship-it <741076@student.dusd.net>
2026-01-06 13:07:17 -0800

    Scope and Access Modifiers
    Completed scopes and access modifiers.

---
+++ P6AlvarezScriptingProject/Assets/Scripts/Looping Scripts/DoWhileLoops.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("The new meme in 6......7");
+        } while (shouldContinue == true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
